90f64614f19ecef16b9b6307bd9a72b4c1f787a0
Sandro Mancuso <sandromancuso@yahoo.com>
2011-07-17 00:38:20 +0100

    Intermidiary code and tests removed.

--- src/test/java/org/craftedsw/harddependencies/TripServiceTest.java
+++ src/test/java/org/craftedsw/harddependencies/TripServiceTest.java
@@ -4,62 +4,59 @@ import static org.craftedsw.harddependencies.UserBuilder.anUser;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
 import org.craftedsw.harddependencies.exception.UserNotLoggedInException;
 import org.craftedsw.harddependencies.trip.Trip;
+import org.craftedsw.harddependencies.trip.TripService;
 import org.craftedsw.harddependencies.user.User;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TripServiceTest {
 	
-	private static final User UNUSED_USER = null;
-	private static final User NO_LOGGED_USER = null;
-	private User loggedUser = new User();
-	private User targetUser = new User();
+	private User NON_LOGGED_USER = null;
+	private User UNUSED_USER = new User();
+	private User LOGGED_USER = new User();
+	
 	private TripService tripService;
-
+	
 	@Before
 	public void initialise() {
 		tripService = createTripService();
 	}
 	
-	@Test(expected=UserNotLoggedInException.class) public void 
-	shouldThrowExceptionWhenUserIsNotLoggedIn() throws Exception {
-		loggedUser = NO_LOGGED_USER;
-		 
-		tripService.getTripsByUser(UNUSED_USER);
+	@Test(expected = UserNotLoggedInException.class) public void 
+	shouldThrowExceptionIfUserIsNotLoggedIn() throws Exception {
+		tripService.getFriendTrips(NON_LOGGED_USER, UNUSED_USER);
 	}
 	
 	@Test public void 
-	shouldNotReturnTripsWhenLoggedUserIsNotAFriend() throws Exception {		
-		List<Trip> trips = tripService.getTripsByUser(targetUser);
-		 
-		assertThat(trips.size(), is(equalTo(0)));
+	shouldNotReturnTripsWhenLoggedUserIsNotAFriend() throws Exception {
+		List<Trip> friendTrips = tripService.getFriendTrips(LOGGED_USER, anUser().build());
+		
+		assertTrue(friendTrips.isEmpty());
 	}
 	
 	@Test public void 
 	shouldReturnTripsWhenLoggedUserIsAFriend() throws Exception {
-		User john = anUser().friendsWith(loggedUser)
-					        .withTrips(new Trip(), new Trip())
-					        .build();
-		 
-		List<Trip> trips = tripService.getTripsByUser(john);
-		 
-		assertThat(trips, is(equalTo(john.trips())));
+		User john = anUser().friendsWith(LOGGED_USER)
+				            .withTrips(new Trip(), new Trip())
+				            .build();
+
+		List<Trip> friendTrips = tripService.getFriendTrips(LOGGED_USER, john);
+		
+		assertThat(friendTrips, is(equalTo(john.trips())));
 	}
 
-	private TripService createTripService() {
+	protected TripService createTripService() {
 		return new TripService() {
-			@Override protected User loggedUser() {
-				return loggedUser;
-			}
-			@Override protected List<Trip> findTripsByUser(User user) {
+			@Override protected List<Trip> findTripsForFriend(User user) {
 				return user.trips();
 			}
 		};
 	}
-	
+
 }
